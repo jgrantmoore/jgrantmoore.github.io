09ada9d1e332c4abdcf00585d5e19628250fac84
James (Grant) Moore <107823094+jgrantmoore@users.noreply.github.com>
2024-02-13 11:10:10 -0500

    Fixed Title
    Title was displaying "React App", Fixed this issue

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import { Layout } from './Layout';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <HashRouter>
+        <title>Grant Moore</title>
         <div>
             <React.StrictMode>
                 <Layout>
