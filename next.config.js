4ad54834ba3ba5bdb6cc25f219f842b9713315e9
James (Grant) Moore <107823094+jgrantmoore@users.noreply.github.com>
2026-01-26 11:05:36 -0500

    Updated CSS Issues (WIP)
    CSS Issues update. Mainly pushing this so I can revert to it in case what I'm about to try doesn't work.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '',
-  trailingSlash: true,
+  /**
+   * Static Export
+   *
+   * Sets the build to static export so that we can
+   * host from any static html hosting service.
+   *
+   * Limitations: https://nextjs.org/docs/pages/guides/static-exports
+   */
+
+  output: "export",
+  basePath: '', // No basePath needed for user/org sites with custom domain
   images: {
+    // Allows next/image to be used without 3rd party optimization service. - necessary for static export
     unoptimized: true,
   },
-};
-
-module.exports = nextConfig;
+}
